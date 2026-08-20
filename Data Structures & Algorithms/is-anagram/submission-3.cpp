62f2da9cc4bf5b6893ee463635d47852f7d5a858
Dulana <91260199+DulanaW@users.noreply.github.com>
2026-08-20 08:06:44 +0530

    Add: is-anagram - submission-3

---
+++ Data Structures & Algorithms/is-anagram/submission-3.cpp
@@ -0,0 +1,30 @@
+class Solution {
+public:
+    bool isAnagram(string s, string t) {
+        if (s.length() != t.length()) return false;
+
+        unordered_map<char, int> counter;
+        for (char i : s) {
+            if (counter.contains(i)) {
+                counter.at(i)++;
+            } else {
+                counter.insert({i, 1});
+            }
+        }
+        for (char i : t) {
+            if (counter.contains(i)) {
+                counter.at(i)--;
+                if (counter.at(i) < 0) {
+                    return false;
+                } 
+            } else {
+                return false;
+            }
+        } 
+        for (char i : s) {
+            if (counter.at(i)) {
+                return false;
+            } 
+        } return true;
+    }
+};
